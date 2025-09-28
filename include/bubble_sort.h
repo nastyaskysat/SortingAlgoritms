ee6b042130d0a5fea3e2c16a6e2971be4a432356
nastyaskysat <nastyaskysat11@gmail.com>
2025-09-28 22:31:50 +0300

    Исправила синтаксические ошибки

--- include/bubble_sort.h
+++ include/bubble_sort.h
@@ -5,13 +5,14 @@
 template <typename RandomIT>
 void bubble_sort(RandomIT begin, RandomIT end)
 {
-    for (auto j = begin; j!=end-1; ++j){
-    for (auto i = begin; i != end; i++)
+    for (auto j = begin; j != end - 1; ++j)
     {
-        if (*i>*(i+1)){
-            std::iter_swap(i, i+1);
+        for (auto i = begin; i != end; i++)
+        {
+            if (*i > *(i + 1))
+            {
+                std::iter_swap(i, i + 1);
+            }
         }
-    }
-       
     }
 }
